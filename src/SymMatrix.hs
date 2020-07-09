b788ced837f8898a2b6f725ca63e2d3fe350cb14
Ward Wheeler <wheeler@amnh.org>
2020-07-09 11:40:30 -0400

    added update Matrix functions

--- src/SymMatrix.hs
+++ src/SymMatrix.hs
@@ -33,10 +33,6 @@ Maintainer  :  Ward Wheeler <wheeler@amnh.org>
 Stability   :  unstable
 Portability :  portable (I hope)
 
-ToDo 
-    1) update matrix with list of triples (need check within bounds)
-    2) add new row (if not in bounds--rows?)
-
 -}
 
 module SymMatrix (empty, dim, fromLists, Matrix, 
@@ -48,7 +44,11 @@ module SymMatrix (empty, dim, fromLists, Matrix,
 import qualified Data.List as L
 import qualified Data.Vector as V
 import qualified Data.Vector.Generic as G
+import qualified Data.Sort as S
 
+-- | functions for triples
+fst3 :: (a,b,c) -> a
+fst3 (d,_,_) = d
 
 -- | Matrix type as Vector of Vectors
 type Matrix a = V.Vector (V.Vector a)
@@ -135,19 +135,73 @@ checkSymmetry inVV pairList =
         if firstCheck then checkSymmetry inVV (tail pairList)
         else error ("Matrix is not symmetrical:" ++ (show (iIndex, jIndex)) ++ "=>" ++ (show ((inVV V.! iIndex) V.! jIndex)) ++ " /= " ++ (show ((inVV V.! jIndex) V.! iIndex)))
 
--- | updateMatrix takes a list of triples and update matrix
---- updat eall at once checking for bounds
-updateMatrix :: Matrix a -> [(Int, Int, a)] -> Matrix a
-updateMatrix inM modList =
-    if L.null modList then inM
-    else 
-        inM
-
 -- | addMatrixRows add rows to existing matrix to extend Matrix dimention
 addMatrixRows :: (Eq a) => Matrix a -> Matrix a -> Matrix a
 addMatrixRows inM newRows =
     if SymMatrix.null newRows then inM
     else 
         inM V.++ newRows
+
+-- | reIndexTriple taske (i,j,k) and returns (max i j, min i j, k)
+reIndexTriple :: (Ord a) => (a, a, b) -> (a, a, b)
+reIndexTriple trip@(iIndex, jIndex, value) =
+    if iIndex > jIndex then trip
+    else (jIndex, iIndex, value)
+
+-- | updateMatrix takes a list of triples and update matrix
+-- update all at once checking for bounds
+-- could naively do each triple in turn, but would be alot of copying
+updateMatrix :: (Eq a, Show a) => Matrix a -> [(Int, Int, a)] -> Matrix a
+updateMatrix inM modList =
+    if L.null modList then inM
+    else 
+        let orderedTripleList = S.uniqueSortOn fst3 $ fmap reIndexTriple modList
+            minRow = fst3 $ head orderedTripleList
+            maxRow = fst3 $ last orderedTripleList
+        in
+        if maxRow >= rows inM then error ("Update matrix out of bounds, row = " ++ (show $ rows inM) ++ " and trying to update row " ++ (show maxRow))
+        else
+            let firstPart = V.unsafeTake minRow inM
+                restPart  = V.unsafeDrop minRow inM
+                modifiedRemainder = updateRows restPart orderedTripleList minRow 
+            in
+            addMatrixRows firstPart modifiedRemainder
+
+-- | updateRows takes the section of the matrix containing rows that wil be modified
+-- (some not) and modifes or copies rows and rerns a Matrix (vector of roow vectors)
+-- doesn't do "smart" update of rows, but recursively till no more for that row
+updateRows :: (Show a, Eq a) => Matrix a -> [(Int, Int, a)] -> Int -> Matrix a
+updateRows inM tripList currentRow =
+    if L.null tripList then inM
+    else if SymMatrix.null inM then error ("Matrix is empty and there are modifications that remain: " ++ (show $ tripList))
+    else 
+        let (rowIndex, columnIndex, value) = L.head tripList
+            firstOrigRow = V.head inM
+        in
+        if currentRow /= rowIndex then firstOrigRow `V.cons` (updateRows (V.tail inM) tripList (currentRow + 1))
+        else -- account for multiple modifications to same row
+            let (newRow, newTripList) = modifyRow firstOrigRow columnIndex value currentRow (L.tail tripList)
+            in
+            newRow `V.cons` (updateRows (V.tail inM) newTripList (currentRow + 1))
+
+-- | modifyRow takes an initial modification (column and value) and then checks to see if there are more modifications in that
+-- row (rowNumber) in the remainder of the list of modifications, returning the new row and mod list as a pair
+modifyRow :: V.Vector a -> Int -> a -> Int -> [(Int, Int, a)] -> (V.Vector a, [(Int, Int, a)])
+modifyRow inRow colIndex value rowNumber modList =
+    if colIndex >= (V.length inRow) then error ("Column to modify is outside length of row " ++ (show $ (rowNumber, colIndex)))
+    else 
+        let firstPart = V.unsafeTake colIndex inRow
+            remainderPart = V.unsafeDrop (colIndex + 1) inRow
+            newRow = firstPart V.++ (value `V.cons` remainderPart)
+        in 
+        if L.null modList then (newRow, modList)
+        else 
+            let (nextRowNumber, nextColNumber, nextValue) = L.head modList
+            in
+            if nextRowNumber /= rowNumber then (newRow, modList) 
+            else modifyRow newRow nextColNumber nextValue rowNumber (L.tail modList)
+            
+
+
 
 
